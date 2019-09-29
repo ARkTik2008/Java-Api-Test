1e9ec0010794b0e62d0806e0d78e136ee14a7a5d
Nikolay Malov <n.malov92@gmail.com>
2019-09-29 21:10:42 +0300

    Allure report

--- src/test/java/YandexTranslateApiTest.java
+++ src/test/java/YandexTranslateApiTest.java
@@ -1,9 +1,11 @@
+import io.qameta.allure.Description;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class YandexTranslateApiTest extends TestBase {
 
     @Test
+    @Description("Проверка корректности перевода Автоматизированное тестирование")
     public void shouldGetTranslate() {
         REQUEST.param("lang", "ru-en").
                 param("text", "Автоматизированное тестирование").
@@ -13,6 +15,7 @@ public class YandexTranslateApiTest extends TestBase {
     }
 
     @Test
+    @Description("Проверка получения кода ошибки 501")
     public void shouldHaveStatus501ForIncorrectLang() {
         REQUEST.
                 param("lang", "ru-kkk").
